fba2e6f13e2f1889359e13064efcf3d303ae1fba
Joniel Ibasco <jlibasco@gmail.com>
2013-08-01 02:26:56 +0800

    changed port in webserver

--- scripts/webserver.rb
+++ scripts/webserver.rb
@@ -6,6 +6,6 @@ include WEBrick
 
 directory = Pathname.new(__FILE__).parent.parent.join('build')
 
-s = HTTPServer.new(:Port => 8000,  :DocumentRoot => directory)
+s = HTTPServer.new(:Port => 7777,  :DocumentRoot => directory)
 trap("INT"){ s.shutdown }
 s.start
